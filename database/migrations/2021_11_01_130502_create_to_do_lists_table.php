9fa06f2bdd389d3e33c5489b718884f27e2a73e9
aqilat94 <aqilat94@gmail.com>
2021-11-01 21:13:28 +0800

    create migration for to do list

---
+++ database/migrations/2021_11_01_130502_create_to_do_lists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToDoListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_do_lists', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->date('date')->nullable();
+            $table->boolean('reminder')->default('0');
+
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_do_lists');
+    }
+}
