4d20d8d8dd5f38f7fe11c96f90df1c87489cc401
aqilat94 <aqilat94@gmail.com>
2021-11-01 21:21:26 +0800

    add column is premium

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('phone_no');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_premium')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
