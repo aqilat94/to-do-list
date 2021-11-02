0673231d9a79e3c1c10671150492cb4528f4fe54
aqilat94 <aqilat94@gmail.com>
2021-11-02 16:41:01 +0800

    make route for list

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/index/list', [App\Http\Controllers\ToDoListController::class, 'index'])->name('index:list');
+Route::get('/create/list', [App\Http\Controllers\ToDoListController::class, 'create'])->name('create:list');
+Route::get('/show/list', [App\Http\Controllers\ToDoListController::class, 'show'])->name('show:list');
+Route::post('/store/list', [App\Http\Controllers\ToDoListController::class, 'store'])->name('store:list');
+Route::get('/edit/list/{list}', [App\Http\Controllers\ToDoListController::class, 'edit'])->name('edit:list');
+Route::post('/update/list/{list}', [App\Http\Controllers\ToDoListController::class, 'update'])->name('update:list');
+Route::get('/toggle/list/{list}', [App\Http\Controllers\ToDoListController::class, 'toggle'])->name('toggle:list');
+Route::get('/delete/list/{list}', [App\Http\Controllers\ToDoListController::class, 'destroy'])->name('delete:list');
